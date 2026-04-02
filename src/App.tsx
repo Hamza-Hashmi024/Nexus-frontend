aaa2026bdffdeb9a02084ba943b0aad96e74f0bb
Hamza-Hashmi024 <hamzaasif064@gmail.com>
2026-04-02 23:28:42 +0500

    Define Theme in taliwindConfig.js file

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css'
+import { Button } from '@/components/ui/button'
 
 function App() {
  
@@ -6,6 +7,11 @@ function App() {
   return (
     <div className="bg-red-500 text-white p-10 text-3xl font-bold">
       Comming Soon
+
+
+      <Button variant="outline" className="mt-5"> Click Me </Button>
+       
+      
     </div>
   )
 }
